7e2babba553498d249f549a9cc9d4db1a90c73b9
Ely A Ndiaye <ely.assumpcao.ndiaye@accenture.com>
2020-06-14 04:05:43 -0300

    Adiciona validacoes selecione currences, campo vazio e somente numeros

--- Converter/ViewController/ConverterViewController.swift
+++ Converter/ViewController/ConverterViewController.swift
@@ -16,6 +16,8 @@ class ConverterViewController: UIViewController {
     var textFrom: String = ""
     var textTo: String = ""
     
+    var valueDobule: Double = 0.0
+    
     override func loadView() {
         super.loadView()
         self.view = screenConverter
@@ -33,7 +35,6 @@ class ConverterViewController: UIViewController {
 
 extension ConverterViewController: ExchangeDelegate{
     func pressFromBtn() {
-        print ("From")
         let controller = ConverterListViewController(isSelectedTable: true){ text in
             self.textFrom = text
             self.screenConverter.valueLbl.text = text
@@ -42,7 +43,6 @@ extension ConverterViewController: ExchangeDelegate{
     }
     
     func pressToBtn() {
-        print("To")
         let controller = ConverterListViewController(isSelectedTable: true){ text in
             self.textTo = text
             self.screenConverter.valueToLbl.text = text
@@ -53,19 +53,38 @@ extension ConverterViewController: ExchangeDelegate{
     func ExchangeBtn() {
         print("Exchange")
         
-        let valueFromTextfield = self.screenConverter.valeuFromExchange.text
-        let valueDobule = (valueFromTextfield as! NSString).doubleValue
+        //MARK: -- Validations
+        if (textTo.isEmpty || textFrom.isEmpty) {
+            DisplayTextField(text: "Please select Currences", message: "Por favor informar as moedas de origem e destino")
+            return
+        }
+        
+        guard let valueFromTextfield = self.screenConverter.valeuFromExchange.text, self.screenConverter.valeuFromExchange.text?.count != 0  else {
+            DisplayTextField(text: "Campo vazio", message: "Por favor informar o valor!")
+            return }
+        
+        func verify() -> Bool {
+            if CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: valueFromTextfield)) {
+                return true }
+            else { return false }
+        }
+        
+        if verify(){
+            valueDobule = (valueFromTextfield as! NSString).doubleValue
+        }else{
+            DisplayTextField(text: "Verify Numbuer", message: "Please only numbers in field!")
+            return
+        }
         
         
         viewModel.getDataFromAPI { (item) in
             //MARK: -- DispatchBackground - CoreData DispatchQueue.global(qos: .background).async
             DispatchQueue.main.async {
                 if let liveQuotes = item.quotes, !liveQuotes.isEmpty {
-                    //validar inputs...
-                    self.viewModel.convertValue(item: item, from: self.textFrom, to: self.textTo, value: valueDobule)
+                    self.viewModel.convertValue(item: item, from: self.textFrom, to: self.textTo, value: self.valueDobule)
                     self.screenConverter.finalValue.text = String(format: "%.2f", self.viewModel.result) + self.textTo
                 } else {
-                    print("ApagA")
+                    self.DisplayTextField(text: "Oppss", message: "Please verify you connection or enter on contact by Emaill")
                 }
             }
             
