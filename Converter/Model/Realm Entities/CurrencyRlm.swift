d7032c79a7ed081c02c7d3db2ab6105821fbe699
Ely A Ndiaye <ely.assumpcao.ndiaye@accenture.com>
2020-12-09 17:40:18 -0300

    Altera pbx

--- Converter/Model/Realm Entities/CurrencyRlm.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CurrencyRlm.swift
-//  Converter
-//
-//  Created by ely.assumpcao.ndiaye on 15/07/20.
-//  Copyright © 2020 ely.assumpcao.ndiaye. All rights reserved.
-//
-
-import Foundation
